1e022a39d412f46e735a5426386afe5cd2bf252f
K3666 <68623052+K3666@users.noreply.github.com>
2021-05-14 22:59:45 +0000

    Update solution-3.py

--- solution-3.py
+++ solution-3.py
@@ -1,23 +1,23 @@
-from bs4 import BeautifulSoup
+from bs4 import beautifulSoup 
 from urllib.request import Request, urlopen
-
-url='http://api.ipstack.com/156.209.145.228?access_key=2390f189d22408b2db29e7524d305658'
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-a=urlopen(req)
-b=a.read()
-soup=BeautifulSoup(b.decode(),'html.parser')
-t=str(soup)
-c=t.strip(',')
-ss=list(c)
+url = 'http://api.ipstack.com/156.209.145.228?access_key=2390f189d22408b2db29e7524d305658'
+rep = request(url, headers={'User-agent':'Mozilla/5.0'})
+a = urlopen(req)
+b = a.read()
+soup = BeautifulSoup(b.decode(),'html.parser')
+t = str(soup)
+c = t.strip(',')
+ss = list(c)
 for i in c.split(','):
-    x=i.strip('\\')
-    r=x.strip('"')
-    io=r.replace('"','')
-    fo=io.replace('}','')
-    mo=fo.replace('{','')
-    zo=mo.replace(']','')
-    vv=zo.replace('/','')
-    vvv=vv.replace('[','')
+    x = i.strip('\\')
+    r = x.strip('''')
+    io = r.replace("",")
+    fo = io.replace('}',")
+    mo = fo.replace('{',")
+    zo = mo.replace(']',")
+    vv = zo.replace('/',")
+    vvv = vv.replace('[',")
     print('='*50)
-    print(vvv+'    ')
-print('='*50)
+    print(vvv+'  ')
+    print('='*50)
+              
