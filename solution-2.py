ed72acf0920d0d562d2c44d064f0a932eecef44c
K3666 <68623052+K3666@users.noreply.github.com>
2021-05-29 23:11:12 +0100

    Update solution-2.py

--- solution-2.py
+++ solution-2.py
@@ -24,17 +24,17 @@ while true:
     num2 = float(input("Enter second number: "))
     
     if choice == '1':
-      print(num1, "+", num2, "=", add(num1, num2))
+        print(num1, "+", num2, "=", add(num1, num2))
      
-      elif choice == '2':
+    elif choice == '2':
         print(num1, "-", num2, "=", subtract(num1, num2))
         
-        elif choice == '3':
-          print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
           
-          elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
           break
-           else:
-              print("invalid input")
+    else:
+        print("invalid input ")
           
