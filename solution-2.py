12bccaf944967d27add62d479147918e9f439144
K3666 <68623052+K3666@users.noreply.github.com>
2021-06-03 23:33:06 +0100

    Update solution-2.py

--- solution-2.py
+++ solution-2.py
@@ -17,22 +17,22 @@ print("4.divide")
 
 while "true":
   # take input from the user
-  choice = ("Enter choice(1/2/3/4): ")
-  # check if choice is one of the four options
-  if choice in ('1', '2', '3', '4'):
+  Z = input("Enter Z(1/2/3/4): ")
+  # check if Z is one of the four options
+  if Z in ('1', '2', '3', '4'):
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     
-    if choice == '1':
+    if Z == '1':
         print(num1, "+", num2, "=", add(num1, num2))
      
-    elif choice == '2':
+    elif Z == '2':
         print(num1, "-", num2, "=", subtract(num1, num2))
         
-    elif choice == '3':
+    elif Z == '3':
         print(num1, "*", num2, "=", multiply(num1, num2))
           
-    elif choice == '4':
+    elif Z == '4':
         print(num1, "/", num2, "=", divide(num1, num2))
     break
   else:
