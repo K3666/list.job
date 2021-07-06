44d169cd017d3f5f2e3f9ae3e07c8ce7fed9b754
K3666 <68623052+K3666@users.noreply.github.com>
2021-07-07 00:43:09 +0100

    Update solution-2.py

--- solution-2.py
+++ solution-2.py
@@ -5,16 +5,16 @@ print("3.Multiply")
 print("4.Divide")
 while true:
   choice = input("Enter choice(1/2/3/4): ")
-    if choice in ('1','2','3','4'):
-      num1 = float(input("Enter first number :"))
-      num2 = float(input("Enter second number :))
-    if choice == '1':
-       print(num1 + num2)
-    elif choice == '2':                  
-       print(num1 - num2)
-    elif choice == '3' :
-       print(num1 * num2)
-    elif choice == '4' :
-       print(num1 / num2)
-     else:
-       print("invalid ")                  
+  if choice in ('1','2','3','4'):
+     num1 = float(input("Enter first number :"))
+     num2 = float(input("Enter second number :))
+  if choice == '1':
+     print(num1 + num2)
+  elif choice == '2':                  
+     print(num1 - num2)
+  elif choice == '3' :
+     print(num1 * num2)
+  elif choice == '4' :
+     print(num1 / num2)
+  else:
+    print("invalid ")                  
