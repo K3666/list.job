64d4ac6f4a62f0a293bb7ed97d4efddf6aa8223a
K3666 <68623052+K3666@users.noreply.github.com>
2021-07-07 00:54:27 +0100

    Update solution-2.py

--- solution-2.py
+++ solution-2.py
@@ -7,7 +7,7 @@ while true:
   choice = input("Enter choice(1/2/3/4): ")
   if choice in ('1','2','3','4'):
      num1 = float(input("Enter first number :"))
-     num2 = float(input("Enter second number :))
+     num2 = float(input("Enter second number :"))
   if choice == '1':
      print(num1 + num2)
   elif choice == '2':                  
