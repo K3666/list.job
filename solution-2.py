bb315bcdfb25ea77d3df1ad130dcc90edd674a51
K3666 <68623052+K3666@users.noreply.github.com>
2021-07-28 22:07:51 +0100

    Update solution-2.py

--- solution-2.py
+++ solution-2.py
@@ -3,7 +3,7 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-while true:
+while True:
   choice = input("Enter choice(1/2/3/4): ")
   if choice in ('1','2','3','4'):
      num1 = float(input("Enter first number :"))
